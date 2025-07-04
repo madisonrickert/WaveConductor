0e667eb3feb09e00dc06aad29874eb1de130fcb7
Madison Rickert <3495636+madisonrickert@users.noreply.github.com>
2025-07-03 17:39:54 -0700

    Simplify and clean up homepage

--- src/routes/homePage.tsx
+++ src/routes/homePage.tsx
@@ -3,79 +3,50 @@ import { Link } from "react-router";
 import { FaPlay } from "react-icons/fa";
 
 export function HomePage() {
+    function renderHighlight(name: string, imageUrl: string, linkUrl?: string) {
+        const Wrapper: any = linkUrl ? "a" : Link;
+        const wrapperProps = linkUrl
+            ? { className: "work-highlight-link", href: linkUrl, target: "_blank" }
+            : { className: "work-highlight-link", to: `/${name.toLowerCase()}` };
+
+        return (
+            <figure className="work-highlight work-grid-item" key={name}>
+                <Wrapper {...wrapperProps}>
+                    <div className="work-highlight-image">
+                        <img className="full-width" src={imageUrl} />
+                        <span className="work-highlight-name">{name}</span>
+                        <div className="work-highlight-sheen sheen-on-hover">
+                            <FaPlay />
+                        </div>
+                    </div>
+                </Wrapper>
+            </figure>
+        );
+    }
+
     return (
         <div className="homepage">
             <main className="content">
-                <WorkSection />
+                <section className="content-section work work-grid" id="work">
+                    {renderHighlight("Cymatics", "/assets/images/cymatics5_cropped.jpg")}
+                    {renderHighlight("Line", "/assets/images/gravity4_cropped.jpg")}
+                    {renderHighlight("Flame", "/assets/images/flame.jpg")}
+                    {renderHighlight("Dots", "/assets/images/dots2.jpg")}
+                    {renderHighlight("Waves", "/assets/images/waves2.jpg")}
+                    {renderHighlight("Mito", "/assets/images/mito_cover.png", 'https://hellochar.github.io/mito/#/')}
+                </section>
             </main>
-            <Footer />
+            <footer className="page-footer">
+                <div className="copyright">
+                    <h3>Credits & Copyright:</h3>
+                    <ul>
+                        <li><a href="https://github.com/hellochar">Xiaohan Zhang</a></li>
+                        <li><a href="https://joshrickert.com">Madison Rickert</a></li>
+                        <li><a href="https://lovetech.org">Rich Trapani / LoveTech</a></li>
+                    </ul>
+                </div>
+            </footer>
         </div>
     );
 }
 
-function WorkSection() {
-    return (
-        <section className="content-section work" id="work">
-            {renderHighlight("Cymatics", "/assets/images/cymatics5_cropped.jpg")}
-            {renderHighlight("Line", "/assets/images/gravity4_cropped.jpg")}
-            {renderHighlight("Flame", "/assets/images/flame.jpg")}
-            {renderHighlight("Dots", "/assets/images/dots2.jpg")}
-            {renderHighlight("Waves", "/assets/images/waves2.jpg")}
-            {renderHighlight("Mito", "/assets/images/mito_cover.png", 'https://hellochar.github.io/mito/#/')}
-        </section>
-    );
-}
-
-function Footer() {
-    return (
-        <footer className="page-footer">
-            <div className="copyright">
-                &copy; 2013 - present Xiaohan Zhang
-            </div>
-        </footer>
-    );
-}
-
-function renderHighlight(name: string, imageUrl: string, linkUrl?: string) {
-    const hasCustomURL = linkUrl != null;
-    let innerEl: React.JSX.Element;
-    if (hasCustomURL) {
-        innerEl = (
-            <>
-                <figcaption>
-                    <a className="work-highlight-name" href={linkUrl} target="_blank">{name}</a>
-                </figcaption>
-                <a href={linkUrl} target="_blank">
-                    <div className="work-highlight-image">
-                        <img className="full-width" src={imageUrl} />
-                        <div className="work-highlight-sheen sheen-on-hover">
-                            <FaPlay />
-                        </div>
-                    </div>
-                </a>
-            </>
-        );
-    } else {
-        linkUrl = `/${name.toLowerCase()}`;
-        innerEl = (
-            <>
-                <figcaption>
-                    <Link className="work-highlight-name" to={linkUrl}>{name}</Link>
-                </figcaption>
-                <Link to={linkUrl}>
-                    <div className="work-highlight-image">
-                        <img className="full-width" src={imageUrl} />
-                        <div className="work-highlight-sheen sheen-on-hover">
-                            <FaPlay />
-                        </div>
-                    </div>
-                </Link>
-            </>
-        );
-    }
-    return (
-        <figure className="work-highlight">
-            {innerEl}
-        </figure>
-    );
-}
