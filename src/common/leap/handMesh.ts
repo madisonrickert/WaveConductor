b02f423b8981f7d8ef400b79c4754275d878156c
Madison Rickert <3495636+madisonrickert@users.noreply.github.com>
2025-07-07 16:27:33 -0700

    Optimizations to handMesh code. Commented out finger rendering code that wasn't even working anyway.

--- src/common/leap/handMesh.ts
+++ src/common/leap/handMesh.ts
@@ -2,36 +2,40 @@ import * as Leap from "leapjs";
 import * as THREE from 'three';
 import { mapLeapToThreePosition } from './util';
 
-export class HandMesh extends THREE.Object3D {
+// Consider cross-referencing with this plugin:
+// https://github.com/leapmotion/leapjs-plugins/blob/master/main/bone-hand/leap.bone-hand.js
+
+export class HandMesh extends THREE.Group {
     static boneGeometry = new THREE.SphereGeometry(10, 3, 3);
     static boneMeshMaterial = new THREE.MeshBasicMaterial({
         color: 0xadd6b6,
         wireframeLinewidth: 5,
         wireframe: true,
     });
-    static boneLineMaterial = new THREE.LineBasicMaterial({
-        color: 0xadd6b6,
-        linewidth: 5,
-    });
+    // static boneLineMaterial = new THREE.LineBasicMaterial({
+    //     color: 0xadd6b6,
+    //     linewidth: 5,
+    // });
 
     private bones: { [id: string]: THREE.Mesh } = {};
-    private fingers: { [id: string]: THREE.Line } = {};
+    // private fingers: { [id: string]: THREE.Line } = {};
 
     constructor() {
         super();
         this.name = 'Hand';
     }
 
-    private addFinger(fingerType: number): THREE.Line {
-        if (!this.fingers["finger" + fingerType]) {
-            const fingerLine = new THREE.Line(new THREE.BufferGeometry(), HandMesh.boneLineMaterial);
-            this.fingers["finger" + fingerType] = fingerLine;
-            this.add(fingerLine);
-        }
-        return this.fingers["finger" + fingerType];
-    }
+    // private getFingerLine(fingerType: number): THREE.Line {
+    //     const id = "finger" + fingerType;
+    //     if (!this.fingers[id]) {
+    //         const fingerLine = new THREE.Line(new THREE.BufferGeometry(), HandMesh.boneLineMaterial);
+    //         this.fingers[id] = fingerLine;
+    //         this.add(fingerLine);
+    //     }
+    //     return this.fingers[id];
+    // }
 
-    private addBone(fingerType: number, boneType: number): THREE.Mesh {
+    private getBoneMesh(fingerType: number, boneType: number): THREE.Mesh {
         const id = `${fingerType},${boneType}`;
         if (!this.bones[id]) {
             const boneMesh = new THREE.Mesh(HandMesh.boneGeometry, HandMesh.boneMeshMaterial);
@@ -41,35 +45,36 @@ export class HandMesh extends THREE.Object3D {
         return this.bones[id];
     }
 
-    private updateBonePosition(fingerType: number, boneType: number, position: THREE.Vector3) {
+    private updateBonePosition(fingerType: number, boneType: number, x: number, y: number, z: number) {
         const id = `${fingerType},${boneType}`;
         if (this.bones[id]) {
-            this.bones[id].position.copy(position);
+            this.bones[id].position.set(x, y, z);
         }
     }
 
-    private updateFingerGeometry(fingerType: number, boneType: number, position: THREE.Vector3) {
-        const fingerLine = this.fingers["finger" + fingerType];
-        if (fingerLine) {
-            const fingerGeometry = fingerLine.geometry as THREE.BufferGeometry;
-            const positions = fingerGeometry.getAttribute('position') as THREE.Float32BufferAttribute || 
-                new THREE.Float32BufferAttribute(new Float32Array((boneType + 1) * 3), 3);
-            positions.setXYZ(boneType, position.x, position.y, position.z);
-            fingerGeometry.setAttribute('position', positions);
-        }
-    }
+    // private updateFingerGeometry(fingerType: number, boneType: number, x: number, y: number, z: number) {
+    //     const fingerLine = this.getFingerLine(fingerType);
+    //     const fingerGeometry = fingerLine.geometry;
+    //     let geometryPosition = fingerGeometry.getAttribute('position');
+    //     if (!geometryPosition) {
+    //         geometryPosition = new THREE.Float32BufferAttribute(new Float32Array((boneType + 1) * 3), 3);
+    //         fingerGeometry.setAttribute('position', geometryPosition);
+    //     }
+
+    //     geometryPosition.setXYZ(boneType, x, y, z);
+    //     geometryPosition.needsUpdate = true;
+    // }
 
     public update(canvas: HTMLCanvasElement, hand: Leap.Hand) {
-        hand.fingers.forEach((finger) => {
-            this.addFinger(finger.type);
-            finger.bones.forEach((bone) => {
-                this.addBone(finger.type, bone.type);
+        for (const finger of hand.fingers) {
+            // this.getFingerLine(finger.type);
+            for (const bone of finger.bones) {
+                this.getBoneMesh(finger.type, bone.type);
                 const {x, y} = mapLeapToThreePosition(canvas, bone.center());
-                // @todo Don't create a new Vector3 every frame? Is this called every frame?
-                const position = new THREE.Vector3(x, y, 300); // z is set to 300 to keep it close to the camera
-                this.updateBonePosition(finger.type, bone.type, position);
-                this.updateFingerGeometry(finger.type, bone.type, position);
-            });
-        });
+                const z = 300; // z is set to 300 to keep it close to the camera
+                this.updateBonePosition(finger.type, bone.type, x, y, z);
+                // this.updateFingerGeometry(finger.type, bone.type, x, y, z);
+            }
+        }
     }
 }
