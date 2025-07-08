f89cdb5546497713abc8f92e0b7fdf32db0412bc
Madison Rickert <3495636+madisonrickert@users.noreply.github.com>
2025-07-07 23:51:19 -0700

    Add some pretty graphics to the screensaver

--- src/common/screenSaver/screenSaver.tsx
+++ src/common/screenSaver/screenSaver.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import "./screenSaver.css";
 import screenSaverVideoMP4 from "./capture.mp4";
 import screenSaverVideoWEBM from "./capture.webm";
+import statueSVG from "./statue.svg";
+import handSVG from "./hand.svg";
 
 export interface ScreenSaverProps {
     shouldShow: boolean;
@@ -13,11 +15,14 @@ export class ScreenSaver extends React.Component<ScreenSaverProps> {
     public render() {
         return (
             <div className={classnames("screen-saver", { visible: this.props.shouldShow })}>
-                <video autoPlay muted loop className="screen-saver-video">
+                <video autoPlay muted loop className="video">
                     <source src={screenSaverVideoMP4} type="video/mp4" />
                     <source src={screenSaverVideoWEBM} type="video/webm" />
                     Your browser does not support the video tag.
                 </video>
+                <img src={statueSVG} alt="Statue" className="statue graphic" />
+                <img src={handSVG} alt="Left Hand" className="hand graphic" />
+                <img src={handSVG} alt="Right Hand" className="hand hand-right graphic" />
             </div>
         );
     }
