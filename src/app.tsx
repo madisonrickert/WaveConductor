fb6ac34622ec1e9eb3b494cee85c397947fe8e2a
Madison Rickert <3495636+madisonrickert@users.noreply.github.com>
2026-02-18 22:29:13 -0800

    Remove unused icon context and convert App component to functional component

--- src/app.tsx
+++ src/app.tsx
@@ -1,33 +1,11 @@
-import React from "react";
 import { BrowserRouter } from "react-router";
 
 import { AppRoutes } from "./appRoutes";
 
-// Create a context for reactIconBase
-const ReactIconBaseContext = React.createContext({
-    className: "fa-icon",
-    style: {
-        verticalAlign: "text-top",
-    },
-});
-
-class App extends React.PureComponent<object, object> {
-    render() {
-        return (
-            <BrowserRouter>
-                <ReactIconBaseContext.Provider
-                    value={{
-                        className: "fa-icon",
-                        style: {
-                            verticalAlign: "text-top",
-                        },
-                    }}
-                >
-                    <AppRoutes />
-                </ReactIconBaseContext.Provider>
-            </BrowserRouter>
-        );
-    }
+export default function App() {
+    return (
+        <BrowserRouter>
+            <AppRoutes />
+        </BrowserRouter>
+    );
 }
-
-export default App;
